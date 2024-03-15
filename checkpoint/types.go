5db38e911c09c15432fe2a47ef4cebf316ab05f6
Archidoge <archidoge@riema.xyz>
2024-03-15 00:32:59 -0700

    [Update]Checkpoint

--- checkpoint/types.go
+++ checkpoint/types.go
@@ -7,14 +7,22 @@ type IndexerIdentification struct {
 	MetaProtocol string
 }
 
+// CheckpointFromCommitteeIndexer
 type Checkpoint struct {
-	URL          string
-	Name         string
-	Version      string
-	MetaProtocol string
-	Height       string
-	Hash         string
-	Commitment   string
+	// Hex of the Commitment of the Verkle Tree Root
+	Commitment string `json:"commitment"`
+	// Hex of the BlockHash of the checkpoint
+	Hash string `json:"hash"`
+	// BlockHeight of the checkpoint
+	Height string `json:"height"`
+	// Protocol name used by the indexer, fixed as "BRC-20" now
+	MetaProtocol string `json:"metaProtocol"`
+	// Name of the indexer
+	Name string `json:"name"`
+	// URL of the indexer service
+	URL string `json:"url"`
+	// Version number of the Modular Indexer
+	Version string `json:"version"`
 }
 
 type UploadHistory = map[uint]map[string]bool
