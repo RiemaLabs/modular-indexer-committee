b68c27a34ea6e57b4ba753d5a564fbbc9a4569d6
August <august@riema.xyz>
2024-03-18 21:03:22 +0000

    debug checkpoint.Name

--- checkpoint/reporter.go
+++ checkpoint/reporter.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net"
 	"strconv"
 
 	"github.com/RiemaLabs/indexer-committee/ord"
@@ -36,40 +35,25 @@ func NewCheckpoint(indexID IndexerIdentification, header ord.Header) Checkpoint 
 
 const (
 	// AWS_S3_REGION = "AWS_REGION"
-	AWS_S3_REGION = "us-west-2"
-	AWS_S3_BUCKET = "nubit-modular-indexer"
+	AWS_S3_REGION         = "us-west-2"
+	AWS_S3_BUCKET_INDEXER = "nubit-modular-indexer"
 )
 
-// URI s3://arn:aws:s3:us-west-2:905418332373:accesspoint/ap-indexer
-// We will be using this client everywhere in our code
 var awsS3Client *s3.Client
 
-// OBS: "config" is a key word in "github.com/aws/aws-sdk-go-v2/config", cannot be used as argument name here
 func UploadCheckpoint(history UploadHistory, indexerID IndexerIdentification, checkpoint Checkpoint) {
 	// the SDK uses its default credential chain to find AWS credentials. This default credential chain looks for credentials in the following order:aws.Configconfig.LoadDefaultConfig
 	// creds := credentials.NewStaticCredentialsProvider(your_access_key, your_secret_key, "")
 	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(AWS_S3_REGION))
-	// 	config.WithSharedCredentialsFiles(
-	// 		[]string{"test/credentials", "data/credentials"},
-	// 	),
-	// 	config.WithSharedConfigFiles(
-	// 		[]string{"test/config", "data/config"},
-	// 	)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	awsS3Client = s3.NewFromConfig(cfg)
 	uploader := manager.NewUploader(awsS3Client)
-	// downloader := manager.NewDownloader(awsS3Client)
 
-	mac, err := getMACAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
 	objectKey := fmt.Sprintf("test/checkpoint-%s-%s-%s-%s.json",
-		mac, checkpoint.MetaProtocol, checkpoint.Height, checkpoint.Hash)
+		checkpoint.Name, checkpoint.MetaProtocol, checkpoint.Height, checkpoint.Hash)
 
 	// change format into JSON
 	checkpointJSON, err := json.Marshal(checkpoint)
@@ -89,7 +73,7 @@ func UploadCheckpoint(history UploadHistory, indexerID IndexerIdentification, ch
 	}
 
 	output, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(AWS_S3_BUCKET),
+		Bucket: aws.String(AWS_S3_BUCKET_INDEXER),
 		Key:    aws.String(objectKey),
 		Body:   bytes.NewReader(checkpointJSON),
 	})
@@ -102,23 +86,7 @@ func UploadCheckpoint(history UploadHistory, indexerID IndexerIdentification, ch
 		log.Println("Upload output:", output)
 		history[uint(heightUint)][objectKey] = true
 	}
-}
 
-func getMACAddress() (string, error) {
-	// all interfaces info
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		return "", err
-	}
+	// TODO: upload to DA
 
-	// the first MAC addr of non-vertical interface
-	for _, iface := range interfaces {
-		if iface.Flags&net.FlagUp != 0 && iface.Flags&net.FlagLoopback == 0 {
-			// filter virtual and loop interfaces
-			// println(iface.HardwareAddr.String())
-			return iface.HardwareAddr.String(), nil
-		}
-	}
-
-	return "", fmt.Errorf("no active non-loopback network interface found")
 }
