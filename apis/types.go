b8bc807659dd16ec04c708444aa342e0fa5833dc
archidoge <archidoge@riema.xyz>
2024-03-25 00:50:29 -0700

    fix: apis

--- apis/types.go
+++ apis/types.go
@@ -15,47 +15,52 @@ type OrdTransferJSON struct {
 	ContentType   string       `json:"contentType"`
 }
 
-type BRC20VerifiableCurrentBalanceResult struct {
+type Brc20VerifiableCurrentBalanceResult struct {
 	AvailableBalance string `json:"availableBalance"`
 	OverallBalance   string `json:"overallBalance"`
 }
 
-type Brc20VerifiableGetCurrentBalanceOfWalletResponse struct {
-	Error  string                              `json:"error"`
-	Result BRC20VerifiableCurrentBalanceResult `json:"result"`
-	Proof  string                              `json:"proof"`
+type Brc20VerifiableLatestStateProofResult struct {
+	Keys         []string          `json:"keys"`
+	KeyExists    []bool            `json:"keyExists"`
+	PreValues    []string          `json:"preValues"`
+	PostValues   []string          `json:"postValues"`
+	OrdTransfers []OrdTransferJSON `json:"ordTransfer"`
 }
 
-type BRC20VerifiableCurrentBalanceOfPkscriptResponse struct {
-	Error  string `json:"error"`
-	Result BRC20VerifiableCurrentBalanceResult
-	Proof  string `json:"proof"`
-}
-
-type Brc20VerifiableLatestStateProofResponse struct {
-	Keys       []string          `json:"keys"`
-	KeyExists  []bool            `json:"keyExists"`
-	PreValues  []string          `json:"preValues"`
-	PostValues []string          `json:"postValues"`
-	Proof      string            `json:"proof"`
-	OrdTrans   []OrdTransferJSON `json:"ordTransfer"`
-}
+// Brc20VerifiableGetCurrentBalanceOfWallet
 
 type Brc20VerifiableGetCurrentBalanceOfWalletRequest struct {
 	Tick   string `json:"tick"`
 	Wallet string `json:"wallet"`
 }
 
+type Brc20VerifiableGetCurrentBalanceOfWalletResponse struct {
+	Error  *string                              `json:"error"`
+	Result *Brc20VerifiableCurrentBalanceResult `json:"result"`
+	Proof  *string                              `json:"proof"`
+}
+
+// Brc20VerifiableCurrentBalanceOfPkscript
+
 type Brc20VerifiableCurrentBalanceOfPkscriptRequest struct {
 	Tick     string `json:"tick"`
 	Pkscript string `json:"pkscript"`
 }
 
-type Brc20VerifiableCurrentBalanceOfPkscriptRsponse struct {
-	Error  string `json:"error"`
-	Result struct {
-		AvailableBalance string `json:"availableBalance"`
-		OverallBalance   string `json:"overallBalance"`
-	} `json:"result"`
-	Proof string `json:"proof"`
+type Brc20VerifiableCurrentBalanceOfPkscriptResponse struct {
+	Error  *string `json:"error"`
+	Result *Brc20VerifiableCurrentBalanceResult
+	Proof  *string `json:"proof"`
+}
+
+// Brc20VerifiableLatestStateProof
+
+type Brc20VerifiableLatestStateProofRequest struct {
+}
+
+type Brc20VerifiableLatestStateProofResponse struct {
+	Error  *string `json:"error"`
+	Result *Brc20VerifiableLatestStateProofResult
+	Proof  *string `json:"proof"`
 }
