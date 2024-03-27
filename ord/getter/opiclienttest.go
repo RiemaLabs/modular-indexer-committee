310038c9a826e4c08011855b72b0ff4cabb8d437
august <august@riema.xyz>
2024-03-27 11:01:06 +0000

    fix: modify func NewOPIOrdGetterTest

--- ord/getter/opiclienttest.go
+++ ord/getter/opiclienttest.go
@@ -1,6 +1,10 @@
 package getter
 
 import (
+	"encoding/csv"
+	"os"
+	"strconv"
+
 	"gorm.io/gorm"
 )
 
@@ -8,19 +12,61 @@ type OPIOrdGetterTest struct {
 	db                *gorm.DB
 	LatestBlockHeight uint
 	BlockHash         map[uint]string
+	OrdTransfers      []OrdTransfer
 }
 
 func NewOPIOrdGetterTest(config *DatabaseConfig, latestBlockHeight uint) (*OPIOrdGetterTest, error) {
-	db, err := ConnectOPIDatabase(config)
+	// Open the CSV file
+	file, err := os.Open("./data/785000-getter.csv")
 	if err != nil {
 		return nil, err
 	}
+	defer file.Close()
+
+	// Parse the CSV file
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// Initialize OPIOrdGetterTest struct
 	getter := OPIOrdGetterTest{
-		db:                db,
 		LatestBlockHeight: latestBlockHeight,
 		BlockHash:         make(map[uint]string),
 	}
-	return &getter, err
+
+	// Parse CSV records into OrdTransfer structs
+	for _, record := range records[1:] { // Skip header row
+		// "id","pkscript","wallet","tick","overall_balance","available_balance","block_height","event_id"
+		id, _ := strconv.Atoi(record[0])
+		// pkt := record[1]
+		// wallet := record[2]
+		// tick := record[3]
+		// overall_balance := record[4]
+		// available_balance := record[5]
+		blockHeight, _ := strconv.Atoi(record[6])
+		// eventID, _ := strconv.Atoi(record[7])
+
+		// Create OrdTransfer struct
+		ordTransfer := OrdTransfer{
+			ID:            uint(id),
+			InscriptionID: "inscriptionID",
+			BlockHeight:   uint(blockHeight),
+			OldSatpoint:   "",
+			NewSatpoint:   "",
+			NewPkscript:   "pkt",
+			NewWallet:     "wallet",
+			SentAsFee:     false,
+			Content:       nil,
+			ContentType:   "",
+		}
+
+		// Append to OrdTransfers slice
+		getter.OrdTransfers = append(getter.OrdTransfers, ordTransfer)
+	}
+
+	return &getter, nil
 }
 
 func (opi *OPIOrdGetterTest) GetLatestBlockHeight() (uint, error) {
