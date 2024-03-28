dc685cf46b8d7ae892e664470b3ee44445f2d0c2
august <august@riema.xyz>
2024-03-28 15:52:33 +0000

    fix: add csv files for OrdTransfers slice and block_hashes map; substitute db with filteredOrdTransfers []OrdTransfer in func GetBlockHash & func GetOrdTransfers

--- ord/getter/opiclienttest.go
+++ ord/getter/opiclienttest.go
@@ -1,26 +1,89 @@
 package getter
 
 import (
-	"gorm.io/gorm"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/RiemaLabs/modular-indexer-committee/ord"
 )
 
 type OPIOrdGetterTest struct {
-	db                *gorm.DB
 	LatestBlockHeight uint
 	BlockHash         map[uint]string
+	OrdTransfers      []OrdTransfer
 }
 
 func NewOPIOrdGetterTest(config *DatabaseConfig, latestBlockHeight uint) (*OPIOrdGetterTest, error) {
-	db, err := ConnectOPIDatabase(config)
-	if err != nil {
-		return nil, err
-	}
+	// Initialize OPIOrdGetterTest struct
 	getter := OPIOrdGetterTest{
-		db:                db,
 		LatestBlockHeight: latestBlockHeight,
 		BlockHash:         make(map[uint]string),
 	}
-	return &getter, err
+
+	// read data/*-brc20_block_hashes.csv and populate the BlockHash map in the OPIOrdGetterTest struct
+	file, err := os.Open("./data/782000-brc20_block_hashes.csv")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	for _, record := range records[1:] { // Skip header row
+		blockHeight, _ := strconv.Atoi(record[0])
+		blockHash := record[1]
+		getter.BlockHash[uint(blockHeight)] = blockHash
+	}
+
+	// read data/*-ord_transfers.csv and populate the OrdTransfers slice in the OPIOrdGetterTest struct
+	file, err = os.Open("./data/782000-ord_transfers.csv")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	reader = csv.NewReader(file)
+	records, err = reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse CSV records into OrdTransfer structs
+	// "id","inscription_id","block_height","old_satpoint","new_satpoint","new_pkscript","new_wallet","sent_as_fee","content","content_type"
+	for _, record := range records[1:] { // Skip header row
+		id, _ := strconv.Atoi(record[0])
+		inscriptionID := record[1]
+		blockHeight, _ := strconv.Atoi(record[2])
+		oldSatpoint := record[3]
+		newSatpoint := record[4]
+		newPkscript := ord.Pkscript(record[5])
+		new_wallet := ord.Wallet(record[6])
+		sent_as_fee, _ := strconv.ParseBool(record[7])
+		content := record[8]
+		content_type := record[9]
+
+		// Create OrdTransfer struct
+		ordTransfer := OrdTransfer{
+			ID:            uint(id),
+			InscriptionID: inscriptionID,
+			BlockHeight:   uint(blockHeight),
+			OldSatpoint:   oldSatpoint,
+			NewSatpoint:   newSatpoint,
+			NewPkscript:   newPkscript,
+			NewWallet:     new_wallet,
+			SentAsFee:     bool(sent_as_fee),
+			Content:       []byte(content),
+			ContentType:   content_type,
+		}
+
+		// Append to OrdTransfers slice
+		getter.OrdTransfers = append(getter.OrdTransfers, ordTransfer)
+	}
+
+	return &getter, nil
 }
 
 func (opi *OPIOrdGetterTest) GetLatestBlockHeight() (uint, error) {
@@ -31,34 +94,16 @@ func (opi *OPIOrdGetterTest) GetBlockHash(blockHeight uint) (string, error) {
 	if result, found := opi.BlockHash[blockHeight]; found {
 		return result, nil
 	}
-	var blockHash string
-	sql := `
-		SELECT block_hash
-		FROM block_hashes
-		WHERE block_height = $1
-	`
-	err := opi.db.Raw(sql, blockHeight).Scan(&blockHash).Error
-	if err != nil {
-		return "", err
-	}
-	return blockHash, nil
+	return "", fmt.Errorf("block hash not found for block height: %d", blockHeight)
 }
 
 func (opi *OPIOrdGetterTest) GetOrdTransfers(blockHeight uint) ([]OrdTransfer, error) {
-	var ordTransfers []OrdTransfer
-	sql := `
-		SELECT ot.id, ot.inscription_id, ot.block_height, ot.old_satpoint, ot.new_satpoint, ot.new_pkscript, ot.new_wallet, ot.sent_as_fee, oc."content", oc.content_type
-		FROM ord_transfers ot
-		LEFT JOIN ord_content oc ON ot.inscription_id = oc.inscription_id
-		LEFT JOIN ord_number_to_id onti ON ot.inscription_id = onti.inscription_id
-		WHERE ot.block_height = $1
-			AND onti.cursed_for_brc20 = false
-			AND oc."content" is not null AND oc."content"->>'p' = 'brc-20'
-		ORDER BY ot.id asc;
-		`
-	err := opi.db.Raw(sql, blockHeight).Scan(&ordTransfers).Error
-	if err != nil {
-		return make([]OrdTransfer, 0), err
+	var filteredOrdTransfers []OrdTransfer
+	for _, transfer := range opi.OrdTransfers {
+		// Filter by block height by iterating over OrdTransfers slice
+		if transfer.BlockHeight == blockHeight {
+			filteredOrdTransfers = append(filteredOrdTransfers, transfer)
+		}
 	}
-	return ordTransfers, nil
+	return filteredOrdTransfers, nil
 }
