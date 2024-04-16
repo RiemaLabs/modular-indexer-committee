76872db6b1f885cf3447168868bb75c424da6784
Shuhan Cao <shuhan@riema.xyz>
2024-04-16 14:46:30 -0700

    feat: verify malicious checkpoint experiment

--- maliciousCheckpoint_test.go
+++ maliciousCheckpoint_test.go
@@ -2,25 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/RiemaLabs/modular-indexer-committee/apis"
 	"github.com/RiemaLabs/modular-indexer-committee/ord/getter"
 	"github.com/RiemaLabs/modular-indexer-committee/ord/stateless"
+	"github.com/gin-gonic/gin"
 )
-
-type BlockVerifyData struct {
-	Height              uint
-	VerifyCorrectTime   time.Duration
-	VarifyDuplicateTime time.Duration
-	VarifyOmitTime      time.Duration
-	VarifyChangeTime    time.Duration
-}
 
 func duplicateOrdTransfer(ordTransfers []getter.OrdTransfer) []getter.OrdTransfer {
 	dup := make([]getter.OrdTransfer, len(ordTransfers))
@@ -57,156 +50,116 @@ func changeLastTransactionAmount(ordTransfers []getter.OrdTransfer) []getter.Ord
 	return ordTransfers
 }
 
-func getCorrectProofResp() apis.Brc20VerifiableLatestStateProofResponse {
-	// TODO Get correct proof from header and ordTransfer
+func verifyMaliciousCheckpoint(maliciousFunc func(ordTransfers []getter.OrdTransfer) []getter.OrdTransfer, catchupHeight uint, t *testing.T) (time.Duration, time.Duration, bool) {
+	ordGetter, arguments := loadMain()
+	postQueue, _ := CatchupStage(ordGetter, &arguments, stateless.BRC20StartHeight-1, catchupHeight)
+
+	ordTransfer := postQueue.Header.OrdTrans
+
+	prevQueue, _ := CatchupStage(ordGetter, &arguments, stateless.BRC20StartHeight-1, catchupHeight-1)
+
+	// Get malicious commit
+	header := prevQueue.Header
+	maliciousOrdTransfer := maliciousFunc(ordTransfer)
+	headerMalicious := stateless.Header{
+		Height:         header.Height,
+		Hash:           header.Hash,
+		Root:           header.Root,
+		IntermediateKV: header.IntermediateKV,
+		KV:             header.KV,
+		Access:         header.Access,
+	}
+	stateless.Exec(&headerMalicious, maliciousOrdTransfer, catchupHeight)
+	headerMalicious.Paging(ordGetter, false, stateless.NodeResolveFn)
+	maliciousCommit := headerMalicious.Root.Commit()
+
+	// Get correct proof
+	// Set gin as test mode
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	r.GET("/v1/brc20_verifiable/latest_state_proof", func(c *gin.Context) {
+		apis.GetLatestStateProof(c, postQueue)
+	})
+	// create test server
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+	req, err := http.NewRequest("GET", ts.URL+"/v1/brc20_verifiable/latest_state_proof", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	// check status code
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("[TestGetLatestStateProof]", err)
+	}
+	// Get result
+	var proof apis.Brc20VerifiableLatestStateProofResponse
+	if err := json.Unmarshal(body, &proof); err != nil {
+		log.Fatal("[TestGetLatestStateProof]", err)
+	}
+
+	lastIndex := len(postQueue.History) - 1
+	preState, _ := stateless.Rollingback(postQueue.Header, &postQueue.History[lastIndex])
+	startTime := time.Now()
+	_, err = apis.GeneratePostRoot(preState.Commit(), catchupHeight, &proof)
+	if err != nil {
+		log.Fatal("With error: ", err)
+	} else {
+		log.Println("True checkpoint passed successfully")
+	}
+	endTime := time.Now()
+	verifyCorrectTime := endTime.Sub(startTime)
+
+	findError := false
+	startTime = time.Now()
+	_, err = apis.GeneratePostRoot(maliciousCommit, catchupHeight, &proof)
+	if err != nil {
+		findError = true
+	}
+	endTime = time.Now()
+	verifyMaliciousTime := endTime.Sub(startTime)
+
+	return verifyCorrectTime, verifyMaliciousTime, findError
 }
 
-func verifyMaliciousCheckpoint(ordGetter getter.OrdGetter, arguments *RuntimeArguments, t *testing.T) []BlockVerifyData {
-	blockVerifyData := []BlockVerifyData{}
-	initHeight := stateless.BRC20StartHeight - 1
-	latestHeight := uint(780000)
-	// Fetch the latest block height.
-	header := stateless.LoadHeader(arguments.EnableStateRootCache, initHeight)
-	curHeight := header.Height
-
-	log.Printf("Fast catchup to the lateset block height! From %d to %d \n", curHeight, latestHeight)
-
-	// Create a channel to listen for SIGINT (Ctrl+C) signal
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT)
-
-	// Start to catch-up
-
-	if latestHeight > curHeight {
-		for i := curHeight + 1; i <= latestHeight; i++ {
-			select {
-			case <-sigChan:
-				log.Printf("Saving cache file. Please don't force exit.")
-				stateless.StoreHeader(header, header.Height-2000)
-				os.Exit(0)
-			default:
-				ordTransfer, err := ordGetter.GetOrdTransfers(i)
-				if err != nil {
-					panic(err)
-				}
-
-				// Get duplicate commit
-				duplicatedOrdTransfer := duplicateOrdTransfer(ordTransfer)
-				headerDuplicate := stateless.Header{
-					Height:         header.Height,
-					Hash:           header.Hash,
-					Root:           header.Root,
-					IntermediateKV: header.IntermediateKV,
-					KV:             header.KV,
-					Access:         header.Access,
-				}
-				stateless.Exec(&headerDuplicate, duplicatedOrdTransfer, i)
-				headerDuplicate.Paging(ordGetter, false, stateless.NodeResolveFn)
-				duplicatedCommit := headerDuplicate.Root.Commit()
-
-				// Get omitted commit
-				omittedOrdTransfer := omitOrdTransfer(ordTransfer)
-				headerOmitted := stateless.Header{
-					Height:         header.Height,
-					Hash:           header.Hash,
-					Root:           header.Root,
-					IntermediateKV: header.IntermediateKV,
-					KV:             header.KV,
-					Access:         header.Access,
-				}
-				stateless.Exec(&headerOmitted, omittedOrdTransfer, i)
-				headerOmitted.Paging(ordGetter, false, stateless.NodeResolveFn)
-				omittedCommit := headerOmitted.Root.Commit()
-
-				// Get changed commit
-				changedLastTransactionAmount := changeLastTransactionAmount(ordTransfer)
-				headerChanged := stateless.Header{
-					Height:         header.Height,
-					Hash:           header.Hash,
-					Root:           header.Root,
-					IntermediateKV: header.IntermediateKV,
-					KV:             header.KV,
-					Access:         header.Access,
-				}
-				stateless.Exec(&headerChanged, changedLastTransactionAmount, i)
-				headerChanged.Paging(ordGetter, false, stateless.NodeResolveFn)
-				changedCommit := headerChanged.Root.Commit()
-
-				// Get correct commit
-				stateless.Exec(header, ordTransfer, i)
-				header.Paging(ordGetter, false, stateless.NodeResolveFn)
-				correctCommit := header.Root.Commit()
-
-				// TODO Get correct proof
-				proof := getCorrectProofResp()
-
-				// Verify the correct commit
-				startTime := time.Now()
-				apis.GeneratePostRoot(correctCommit, i, &proof)
-				endTime := time.Now()
-				VerifyCorrectTime := endTime.Sub(startTime)
-
-				// Verify the duplicated commit
-				startTime = time.Now()
-				apis.GeneratePostRoot(duplicatedCommit, i, &proof)
-				endTime = time.Now()
-				VarifyDuplicateTime := endTime.Sub(startTime)
-
-				// Verify the omitted commit
-				startTime = time.Now()
-				apis.GeneratePostRoot(omittedCommit, i, &proof)
-				endTime = time.Now()
-				VarifyOmitTime := endTime.Sub(startTime)
-
-				// Verify the changed commit
-				startTime = time.Now()
-				apis.GeneratePostRoot(changedCommit, i, &proof)
-				endTime = time.Now()
-				VarifyChangeTime := endTime.Sub(startTime)
-
-				blockVerifyData = append(blockVerifyData, BlockVerifyData{
-					Height:              i,
-					VerifyCorrectTime:   VerifyCorrectTime,
-					VarifyDuplicateTime: VarifyDuplicateTime,
-					VarifyOmitTime:      VarifyOmitTime,
-					VarifyChangeTime:    VarifyChangeTime,
-				})
-
-				if i%1000 == 0 {
-					log.Printf("Blocks: %d / %d \n", i, latestHeight)
-					if arguments.EnableStateRootCache {
-						err := stateless.StoreHeader(header, header.Height-2000)
-						if err != nil {
-							panic(err)
-						}
-					}
-				}
-			}
-		}
+// The following three tests are to verify the malicious checkpoint, return the time to verify the correct checkpoint and the malicious checkpoint, also return if the malicious checkpoint is found
+func TestDuplicateCheckpoint(t *testing.T) {
+	verifyCorrectTime, verifyMaliciousTime, findError := verifyMaliciousCheckpoint(duplicateOrdTransfer, 780000, t)
+	log.Println("Verify Correct Commit time: ", verifyCorrectTime)
+	log.Println("Verify Duplicated Commit time: ", verifyMaliciousTime)
+	if findError {
+		log.Println("Find malicious checkpoint successfully")
+	} else {
+		log.Println("Error not found")
 	}
-
-	return blockVerifyData
 }
 
-func TestMaliciousCheckpoint(t *testing.T) {
-	ordGetterTest, arguments := loadMain()
-	blockVerifyData := verifyMaliciousCheckpoint(ordGetterTest, &arguments, t)
-	jsonData, err := json.MarshalIndent(blockVerifyData, "", "    ")
-	if err != nil {
-		log.Println("[Save to JSON] Error: ", err)
-		return
+func TestOmitCheckpoint(t *testing.T) {
+	verifyCorrectTime, verifyMaliciousTime, findError := verifyMaliciousCheckpoint(omitOrdTransfer, 780000, t)
+	log.Println("Verify Correct Commit time: ", verifyCorrectTime)
+	log.Println("Verify Omited Commit time: ", verifyMaliciousTime)
+	if findError {
+		log.Println("Find malicious checkpoint successfully")
+	} else {
+		log.Println("Error not found")
 	}
+}
 
-	fileName := "verify-malicious-checkpoint-data.json"
-	file, err := os.Create(fileName)
-	if err != nil {
-		log.Println("[Create File] Error", err)
-		return
-	}
-	defer file.Close()
-	_, err = file.Write(jsonData)
-	if err != nil {
-		log.Println("[Write File] Error", err)
-		return
+func TestChangeCheckpoint(t *testing.T) {
+	verifyCorrectTime, verifyMaliciousTime, findError := verifyMaliciousCheckpoint(changeLastTransactionAmount, 780000, t)
+	log.Println("Verify Correct Commit time: ", verifyCorrectTime)
+	log.Println("Verify Changed Commit time: ", verifyMaliciousTime)
+	if findError {
+		log.Println("Find malicious checkpoint successfully")
+	} else {
+		log.Println("Error not found")
 	}
 }
