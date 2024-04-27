bf359b6ce0a959bf3f3770e3cd1653c6924c79b6
Shuhan Cao <shuhan@riema.xyz>
2024-04-26 19:48:41 -0700

    test: accelerate checkpoint experiment

--- checkpointExperiment_test.go
+++ checkpointExperiment_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/RiemaLabs/modular-indexer-committee/checkpoint"
+	"github.com/RiemaLabs/modular-indexer-committee/ord"
 	"github.com/RiemaLabs/modular-indexer-committee/ord/stateless"
 	"github.com/ethereum/go-verkle"
 )
@@ -20,7 +21,7 @@ type CheckpointData struct {
 }
 
 func TestCheckpointExperiment(t *testing.T) {
-	checkpointData := CheckpointExperiment(790000, 790100)
+	checkpointData := NewCheckpointExperiment(790000, 790100)
 	jsonData, err := json.MarshalIndent(checkpointData, "", "    ")
 	if err != nil {
 		log.Println("[Save to JSON] Error: ", err)
@@ -46,8 +47,8 @@ func CheckpointExperiment(startHeight uint, endHeight uint) []CheckpointData {
 	// Calculate the next checkpoint using the previous checkpoint and stateDiff.
 	// For each block height, start with the previous checkpoint and apply stateDiff to compute the next checkpoint.
 	// Record the time taken to compute each checkpoint in milliseconds.
+	ordGetterTest, arguments := loadMain(endHeight)
 	for i := startHeight; i <= endHeight; i++ {
-		ordGetterTest, arguments := loadMain(782000)
 		queue, _ := CatchupStage(ordGetterTest, &arguments, stateless.BRC20StartHeight-1, i)
 		indexerID := checkpoint.IndexerIdentification{
 			URL:          GlobalConfig.Service.URL,
@@ -87,8 +88,76 @@ func CheckpointExperiment(startHeight uint, endHeight uint) []CheckpointData {
 	return checkpointData
 }
 
-func TestCatchup(t *testing.T) {
-	ordGetterTest, arguments := loadMain(782000)
-	queue, _ := CatchupStage(ordGetterTest, &arguments, stateless.BRC20StartHeight-1, uint(790000))
-	log.Println(len(queue.History), queue.History)
+func Test_LoadOrdTransfers(t *testing.T) {
+	ord.GenerateOrdTransfers(790100)
+}
+
+func Test_LoadBRC20BlockHashes(t *testing.T) {
+	ord.GenerateBRC20BlockHashes(790100)
+}
+
+func NewCheckpointExperiment(startHeight uint, endHeight uint) []CheckpointData {
+	checkpointData := make([]CheckpointData, 0)
+	// Calculate the next checkpoint using the previous checkpoint and stateDiff.
+	// For each block height, start with the previous checkpoint and apply stateDiff to compute the next checkpoint.
+	// Record the time taken to compute each checkpoint in milliseconds.
+	getter, arguments := loadMain(uint(790100))
+	queue, _ := CatchupStage(getter, &arguments, stateless.BRC20StartHeight-1, startHeight-1)
+	indexerID := checkpoint.IndexerIdentification{
+		URL:          GlobalConfig.Service.URL,
+		Name:         arguments.CommitteeIndexerName,
+		Version:      Version,
+		MetaProtocol: GlobalConfig.Service.MetaProtocol,
+	}
+	log.Println("finish catchup")
+	for i := startHeight; i <= endHeight; i++ {
+		ordTransfer, err := getter.GetOrdTransfers(i)
+		if err != nil {
+			panic(err)
+		}
+
+		startTime := time.Now()
+		stateless.Exec(queue.Header, ordTransfer, i)
+		hash, err := getter.GetBlockHash(i - 1)
+		if err != nil {
+			panic(err)
+		}
+		newDiffState := stateless.DiffState{
+			Height:       i - 1,
+			Hash:         hash,
+			Access:       queue.Header.Access,
+			VerkleCommit: queue.Header.Root.Commit().Bytes(),
+		}
+		copy(queue.History[:], queue.History[1:])
+		queue.History[len(queue.History)-1] = newDiffState
+		commitment := base64.StdEncoding.EncodeToString(newDiffState.VerkleCommit[:])
+		checkpoint.NewCheckpoint(&indexerID, i-1, hash, commitment)
+		stateDiffTime := time.Since(startTime)
+
+		startTime = time.Now()
+		root := verkle.New()
+		for k, v := range queue.Header.KV {
+			root.Insert(k[:], v[:], stateless.NodeResolveFn)
+		}
+		commit := root.Commit().Bytes()
+		commitment = base64.StdEncoding.EncodeToString(commit[:])
+		hash, err = getter.GetBlockHash(i)
+		if err != nil {
+			panic(err)
+		}
+		checkpoint.NewCheckpoint(&indexerID, i-1, hash, commitment)
+		fullStateTime := time.Since(startTime)
+
+		queue.Header.OrdTrans = ordTransfer
+		// header.Height ++
+		queue.Header.Paging(getter, true, stateless.NodeResolveFn)
+
+		checkpointData = append(checkpointData, CheckpointData{
+			Height:        i,
+			stateDiffTime: stateDiffTime / 1000000,
+			fullStateTime: fullStateTime / 1000000,
+		})
+		log.Println("finish height: ", i)
+	}
+	return checkpointData
 }
--- ord/utils.go
+++ ord/utils.go
@@ -1,4 +1,161 @@
 package ord
 
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 // The number of confirmations to be considered immutable and can't be re-organized.
 const BitcoinConfirmations uint = 6
+
+type OrdTransfer struct {
+	ID            uint
+	InscriptionID string
+	BlockHeight   uint
+	OldSatpoint   string
+	NewSatpoint   string
+	NewPkscript   string
+	NewWallet     string
+	SentAsFee     bool
+	Content       []byte
+	ContentType   string
+	ParentID      string
+}
+
+type BRC20Hashes struct {
+	ID          uint
+	BlockHeight uint
+	BlockHash   string
+}
+
+func ConnectDatabase() *gorm.DB {
+	dsn := "host=127.0.0.1 user=postgres password=170501 dbname=postgres port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	return db
+}
+
+func GenerateOrdTransfers(height uint) {
+	db := ConnectDatabase()
+	filename := "./data/" + fmt.Sprintf("%d", height) + "-ord_transfers.csv"
+	csvFile, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	data := [][]string{
+		{"id", "inscription_id", "block_height", "old_satpoint", "new_satpoint", "new_pkscript", "new_wallet", "sent_as_fee", "content", "content_type", "parent_id"},
+	}
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+	for _, record := range data {
+		if err := writer.Write(record); err != nil {
+			panic(err)
+		}
+	}
+
+	sql := `
+			SELECT ot.id, ot.inscription_id, ot.block_height, ot.old_satpoint, ot.new_satpoint, ot.new_pkscript, ot.new_wallet, ot.sent_as_fee, oc."content", oc.content_type, onti.parent_id
+			FROM ord_transfers ot
+			LEFT JOIN ord_content oc ON ot.inscription_id = oc.inscription_id
+			LEFT JOIN ord_number_to_id onti ON ot.inscription_id = onti.inscription_id
+			WHERE ot.block_height = ?
+				AND onti.cursed_for_brc20 = false
+				AND oc."content" is not null AND oc."content"->>'p' = 'brc-20'
+			ORDER BY ot.id asc;
+			`
+
+	for i := 779832; i <= int(height); i++ {
+		var ordTransfers []OrdTransfer
+
+		db.Raw(sql, i).Scan(&ordTransfers)
+
+		if i%100 == 0 {
+			log.Printf("Height: %d, Total: %d\n", i, len(ordTransfers))
+		}
+
+		data = [][]string{}
+
+		for _, d := range ordTransfers {
+			dStr := []string{
+				strconv.FormatUint(uint64(d.ID), 10),
+				d.InscriptionID,
+				strconv.FormatUint(uint64(d.BlockHeight), 10),
+				d.OldSatpoint,
+				d.NewSatpoint,
+				d.NewPkscript,
+				d.NewWallet,
+				strconv.FormatBool(d.SentAsFee),
+				string(d.Content),
+				d.ContentType,
+				d.ParentID,
+			}
+			data = append(data, dStr)
+		}
+
+		for _, record := range data {
+			if err := writer.Write(record); err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	log.Println("Data written into file!")
+}
+
+func GenerateBRC20BlockHashes(height uint) {
+	db := ConnectDatabase()
+	filename := "./data/" + fmt.Sprintf("%d", height) + "-brc20_block_hashes.csv"
+	csvFile, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	data := [][]string{
+		{"block_height", "block_hash"},
+	}
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+	for _, record := range data {
+		if err := writer.Write(record); err != nil {
+			panic(err)
+		}
+	}
+
+	sql := `SELECT block_height, block_hash FROM public.brc20_block_hashes
+	WHERE block_height >= 779832 AND block_height <= ?
+	ORDER BY id ASC;`
+
+	var brc20Hashes []BRC20Hashes
+
+	db.Raw(sql, height).Scan(&brc20Hashes)
+
+	data = [][]string{}
+
+	for _, d := range brc20Hashes {
+		dStr := []string{
+			strconv.FormatUint(uint64(d.BlockHeight), 10),
+			d.BlockHash,
+		}
+		data = append(data, dStr)
+	}
+
+	for _, record := range data {
+		if err := writer.Write(record); err != nil {
+			panic(err)
+		}
+	}
+
+	log.Println("Data written into file!")
+}
