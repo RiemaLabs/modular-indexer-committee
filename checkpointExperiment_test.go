7d5e9b3bcbccda9de889d2e3f4369088503301b2
Shuhan Cao <shuhan@riema.xyz>
2024-04-29 14:43:45 +0800

    test: finish checkpoint experiment

--- checkpointExperiment_test.go
+++ checkpointExperiment_test.go
@@ -16,12 +16,22 @@ import (
 
 type CheckpointData struct {
 	Height        uint
-	stateDiffTime time.Duration
-	fullStateTime time.Duration
+	StateDiffTime time.Duration
+	FullStateTime time.Duration
 }
 
+func Test_LoadOrdTransfers(t *testing.T) {
+	ord.GenerateOrdTransfers(790100)
+}
+
+func Test_LoadBRC20BlockHashes(t *testing.T) {
+	ord.GenerateBRC20BlockHashes(790100)
+}
+
+// The result of StateDiffTime and FullStateTime are in miliseconds.
 func TestCheckpointExperiment(t *testing.T) {
-	checkpointData := NewCheckpointExperiment(790000, 790100)
+	checkpointData := CheckpointExperiment(790000, 790100)
+	log.Println("checkpointData: ", checkpointData)
 	jsonData, err := json.MarshalIndent(checkpointData, "", "    ")
 	if err != nil {
 		log.Println("[Save to JSON] Error: ", err)
@@ -43,60 +53,6 @@ func TestCheckpointExperiment(t *testing.T) {
 }
 
 func CheckpointExperiment(startHeight uint, endHeight uint) []CheckpointData {
-	checkpointData := make([]CheckpointData, 0)
-	// Calculate the next checkpoint using the previous checkpoint and stateDiff.
-	// For each block height, start with the previous checkpoint and apply stateDiff to compute the next checkpoint.
-	// Record the time taken to compute each checkpoint in milliseconds.
-	ordGetterTest, arguments := loadMain(endHeight)
-	for i := startHeight; i <= endHeight; i++ {
-		queue, _ := CatchupStage(ordGetterTest, &arguments, stateless.BRC20StartHeight-1, i)
-		indexerID := checkpoint.IndexerIdentification{
-			URL:          GlobalConfig.Service.URL,
-			Name:         arguments.CommitteeIndexerName,
-			Version:      Version,
-			MetaProtocol: GlobalConfig.Service.MetaProtocol,
-		}
-		// Calculate the next checkpoint using the previous checkpoint and stateDiff.
-		startTime := time.Now()
-		log.Println(len(queue.History), queue.History)
-		stateDiff := queue.History[len(queue.History)-1]
-		commitment := base64.StdEncoding.EncodeToString(stateDiff.VerkleCommit[:])
-		checkpoint.NewCheckpoint(&indexerID, stateDiff.Height, stateDiff.Hash, commitment)
-		stateDiffTime := time.Since(startTime)
-
-		// Calculate the next checkpoint using the full state.
-		startTime = time.Now()
-		root := verkle.New()
-		for k, v := range queue.Header.KV {
-			root.Insert(k[:], v[:], stateless.NodeResolveFn)
-		}
-		commit := root.Commit().Bytes()
-		commitment = base64.StdEncoding.EncodeToString(commit[:])
-		hash, err := ordGetterTest.GetBlockHash(i)
-		if err != nil {
-			panic(err)
-		}
-		checkpoint.NewCheckpoint(&indexerID, i, hash, commitment)
-		fullStateTime := time.Since(startTime)
-
-		checkpointData = append(checkpointData, CheckpointData{
-			Height:        i,
-			stateDiffTime: stateDiffTime / 1000000,
-			fullStateTime: fullStateTime / 1000000,
-		})
-	}
-	return checkpointData
-}
-
-func Test_LoadOrdTransfers(t *testing.T) {
-	ord.GenerateOrdTransfers(790100)
-}
-
-func Test_LoadBRC20BlockHashes(t *testing.T) {
-	ord.GenerateBRC20BlockHashes(790100)
-}
-
-func NewCheckpointExperiment(startHeight uint, endHeight uint) []CheckpointData {
 	checkpointData := make([]CheckpointData, 0)
 	// Calculate the next checkpoint using the previous checkpoint and stateDiff.
 	// For each block height, start with the previous checkpoint and apply stateDiff to compute the next checkpoint.
@@ -134,6 +90,10 @@ func NewCheckpointExperiment(startHeight uint, endHeight uint) []CheckpointData 
 		checkpoint.NewCheckpoint(&indexerID, i-1, hash, commitment)
 		stateDiffTime := time.Since(startTime)
 
+		queue.Header.OrdTrans = ordTransfer
+		// header.Height ++
+		queue.Header.Paging(getter, true, stateless.NodeResolveFn)
+
 		startTime = time.Now()
 		root := verkle.New()
 		for k, v := range queue.Header.KV {
@@ -148,14 +108,10 @@ func NewCheckpointExperiment(startHeight uint, endHeight uint) []CheckpointData 
 		checkpoint.NewCheckpoint(&indexerID, i-1, hash, commitment)
 		fullStateTime := time.Since(startTime)
 
-		queue.Header.OrdTrans = ordTransfer
-		// header.Height ++
-		queue.Header.Paging(getter, true, stateless.NodeResolveFn)
-
 		checkpointData = append(checkpointData, CheckpointData{
 			Height:        i,
-			stateDiffTime: stateDiffTime / 1000000,
-			fullStateTime: fullStateTime / 1000000,
+			StateDiffTime: stateDiffTime / 1000000,
+			FullStateTime: fullStateTime / 1000000,
 		})
 		log.Println("finish height: ", i)
 	}
